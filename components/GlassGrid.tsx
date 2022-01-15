f35d44b6cbfd7240acb0f2c11b7c61aa040de986
Jacob Fowles <js.fowles@gmail.com>
2022-01-14 19:03:36 -0700

    way messed up

--- components/GlassGrid.tsx
+++ components/GlassGrid.tsx
@@ -7,28 +7,26 @@ interface Props {
 
 const GlassGrid = ({ images }: Props) => {
   return (
-    <div className="p-5 md:px-0" ref={ref}>
+    <div className="p-5 md:px-0">
       <div className="flex flex-col lg:flex-row w-full relative">
         <div className="w-full h-full backdrop-blur-sm bg-black/10 rounded-3xl absolute inset-0 z-10" />
 
-
-          <div className="relative gap-16 p-5 lg:p-16 z-50 grid grid-cols-1 lg:grid-cols-3 w-full">
-            {images.map((image, index) => (
-              <figure key={index}>
-                <Image
-                  priority
-                  alt="Bloop Troop cards"
-                  height={1456}
-                  width={1036}
-                  layout="intrinsic"
-                  src={image}
-                  className="rounded-xl"
-                  objectFit="contain"
-                />
-              </figure>
-            ))}
-          </div>
-        </>
+        <div className="relative gap-16 p-5 lg:p-16 z-50 grid grid-cols-1 lg:grid-cols-3 w-full">
+          {images.map((image, index) => (
+            <figure key={index}>
+              <Image
+                priority
+                alt="Bloop Troop cards"
+                height={1456}
+                width={1036}
+                layout="intrinsic"
+                src={image}
+                className="rounded-xl"
+                objectFit="contain"
+              />
+            </figure>
+          ))}
+        </div>
       </div>
     </div>
   )
