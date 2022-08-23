39e10b1ead3d59817997a27f291710ad163d18bc
Jacob Fowles <js.fowles@gmail.com>
2022-08-23 13:01:08 -0600

    added 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Image from 'next/image';
+import Button from '../components/Button';
+
+const FourOhFour = () => {
+  return (
+    <section className="w-full h-full flex-col-reverse lg:flex-row flex pt-24 relative justify-center">
+      <figure className="w-full scale-x-[-1] -mb-32 drop-shadow-2xl lg:w-1/3 pt-8">
+        <Image
+          layout="responsive"
+          height={1512}
+          objectFit="contain"
+          width={1080}
+          alt="the incredible joel korte is here to say you're lost"
+          src="/images/joel.png"
+        />
+      </figure>
+
+      <div className="lg:w-1/3 flex flex-col items-center justify-center">
+        <h1 className="text-[14vw] h-min">404</h1>
+        <h4 className="pb-8 lg:pb-16">Oh boy... are you in the wrong place.</h4>
+        <Button href="/">Home</Button>
+      </div>
+    </section>
+  );
+};
+
+export default FourOhFour;
